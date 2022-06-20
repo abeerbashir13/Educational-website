40c1e24bc5098830c51003473db0eae81dad9c98
Abeer Abdelrahman Bashir <105046008+abeerbashir13@users.noreply.github.com>
2022-06-21 01:50:51 +0200

    Delete educational website directory

--- educational website/helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'session.php';
-function redirect($url)
-{
-    header("Location: $url");
-    die;
-}
--- educational website/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['email']);
-unset($_SESSION['role']);
-unset($_SESSION['id']);
-session_destroy();
-header("Location: login.php");
-die();
